008317c33ca39367f549790333b6730ec51fe1ec
joeguerra <guerra_joe@hotmail.com>
2018-07-06 22:15:31 -0400

    added doc controller stuff

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -1,15 +1,28 @@
 class DocsController < ApplicationController
+  before_action :find_doc, only: [:show, :edit, :update, :destroy]
+
+
 
 def index
 end
 
 def show
+
 end
 
 def new
+  @doc = Doc.new
 end
 
 def create
+  @doc = Doc.new(doc_params)
+
+  if @doc.save
+    redirect_to @doc
+  else
+    render 'new'
+  end
+
 end
 
 def update
@@ -21,9 +34,12 @@ end
 private
 
 def find_doc
+  @doc = Doc.find(params[:id])
+
 end
 
 def doc_params
+  doc_params.require(:doc).permit(:title, :content)
 end
 
 end
